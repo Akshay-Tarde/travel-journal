8e01c50d553e1c4e49577e1175385b337f4e3017
Akshay-Tarde <akshaymtarde@gmail.com>
2024-12-22 12:57:02 +0530

    Created and styled the header section with the img

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header>
+            <img src="./globe.png" alt="globe icon"></img>
+            <a href="#">mytraveljournal.com</a>
+        </header>
+    )
+}
